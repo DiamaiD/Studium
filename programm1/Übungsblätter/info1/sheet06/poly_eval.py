0386e901fd62c5d86eadc8e5bca49cf31ecd4927
vg-PC\vg <viktor.gange@gmail.com>
2019-11-26 21:19:19 +0100

    no message

--- programm1/Übungsblätter/info1/sheet06/poly_eval.py
+++ programm1/Übungsblätter/info1/sheet06/poly_eval.py
@@ -11,24 +11,18 @@ def poly_eval (p: list, x: float) -> float :
 
 def poly_eval_faster (p: list, x: float) -> float :
     result = 0
+    fasterbase = 1
     for a in p :
-        fasterbase = 0
-        for i in range(p.index(a)+1):
-            if i == 0:
-                fasterbase = 1
-            else:
-                fasterbase = fasterbase * x
         result = result + a * fasterbase
+        fasterbase = fasterbase * x
     return result
 
 def poly_eval_horner(p: list, x: float) -> float:
     result = 0
     umkehrliste = p[::-1]
-    for a in umkehrliste:
-        if (umkehrliste.index(a)+1) == len(p):
-            result = result + a
-        else:
-            result = (result + a)*x
+    for a in umkehrliste[:-1]:
+        result = (result + a)*x
+    result = result + umkehrliste[-1]
     return result 
 
 def time_poly_eval(name: str):
@@ -42,17 +36,13 @@ def time_poly_eval(name: str):
     testliste = testliste+"]"
     if name == "poly_eval":
         teststring = "poly_eval("+testliste+",2)"
-        print(timeit.timeit(teststring, 
-            setup = s, number = 1000))
+        print("Laufzeit von popy_eval:",timeit.timeit(teststring, 
+            setup = s, number = 1000),"Sekunden")
     if name == "poly_eval_faster":
         teststring = "poly_eval_faster("+testliste+",2)"
-        print(timeit.timeit(teststring, 
-            setup = s, number = 1000))
+        print("Laufzeit von popy_eval_faster:",timeit.timeit(teststring, 
+            setup = s, number = 1000),"Sekunden")
     if name == "poly_eval_horner":
         teststring = "poly_eval_horner("+testliste+",2)"
-        print(timeit.timeit(teststring, 
-            setup = s, number = 1000))
-
-time_poly_eval("poly_eval")
-time_poly_eval("poly_eval_faster")
-time_poly_eval("poly_eval_horner")
+        print("Laufzeit von popy_eval_horner:",timeit.timeit(teststring, 
+            setup = s, number = 1000),"Sekunden")
