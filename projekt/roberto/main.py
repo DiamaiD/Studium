8413fc2bed1f5639592d3af983c7f4dced65dc26
vg-PC\vg <viktor.gange@gmail.com>
2019-12-09 04:58:45 +0100

    anpassungen an dem roberto code

--- projekt/roberto/main.py
+++ projekt/roberto/main.py
@@ -25,6 +25,7 @@ basetime = 0.1
 normalspeed = 75
 turnlow = 15
 speedincrement = 10
+increment_of_increment = 3
 maxspeed = 100
 listenlänge = 5
 wandabstand = 20
@@ -33,14 +34,16 @@ def run():
     global speed1, speed2
     left_motor.run_direct()
     right_motor.run_direct()
-    bahnlinie = []
-    for i in range(listenlänge):
-        bahnlinie.append(1)
+    # bahnlinie = []
+    # for i in range(listenlänge):
+    #     bahnlinie.append(1)
 
     while True:
         leftlight = ls1.reflected_light_intensity
         rightlight = ls2.reflected_light_intensity
         abstand = us.distance_centimeters
+        speed1 = normalspeed
+        speed2 = normalspeed
         # if leftlight < schwarz or rightlight < schwarz:
         #     bahnlinie.append(1)
         # else:
@@ -55,35 +58,24 @@ def run():
         if leftlight < schwarz and rightlight < schwarz and abstand > wandabstand:
             speed1 = normalspeed
             speed2 = normalspeed
+            speedincrement = 10
         elif leftlight < schwarz and rightlight > schwarz:
-            #speed1 -= ((250)/(speed1))
             speed1 -= speedincrement
             speed2 = normalspeed
-            if speed2 > maxspeed:
-                speed2 = maxspeed
             if speed1 <= 0:
                 speed1 = 10
+            speedincrement += increment_of_increment
         elif leftlight > schwarz and rightlight < schwarz:
             speed1 = normalspeed
             speed2 -= speedincrement
-            #speed2 -= ((250)/(speed2))
-            if speed1 > maxspeed:
-                speed1 = maxspeed
             if speed2 <= 0:
                 speed2 = 10
+            speedincrement += increment_of_increment
         #elif leftlight > schwarz and rightlight > schwarz and weiß == listenlänge:
-        elif leftlight > schwarz and rightlight > schwarz:
-            if abstand < wandabstand:
-                turn()
-            else:
-                speed1 = -30
-                speed2 = -30
-                left_motor.duty_cycle_sp = speed1
-                right_motor.duty_cycle_sp = speed2
-                time.sleep(basetime*4)
-        elif abstand < wandabstand:
-            speed1 = 0
-            speed2 = 0
+        elif leftlight > schwarz and rightlight > schwarz and abstand < wandabstand:
+            turn()
+        else:
+            search_direction()
 
         left_motor.duty_cycle_sp = speed1
         right_motor.duty_cycle_sp = speed2
@@ -107,5 +99,30 @@ def turn():
         if leftlight < schwarz or rightlight < schwarz:
             break
         time.sleep(0.002)
+
+def search_direction():
+    left_motor.duty_cycle_sp = normalspeed
+    right_motor.duty_cycle_sp = normalspeed
+    time.sleep(basetime*4)
+    leftlight = ls1.reflected_light_intensity
+    rightlight = ls2.reflected_light_intensity
+
+    if leftlight > schwarz and rightlight > schwarz:
+        left_motor.duty_cycle_sp = - normalspeed
+        right_motor.duty_cycle_sp = - normalspeed
+        time.sleep(basetime*4.5)
+
+        left_motor.duty_cycle_sp = normalspeed
+        right_motor.duty_cycle_sp = - normalspeed
+        time.sleep(basetime)
+        
+        left_motor.duty_cycle_sp = normalspeed
+        right_motor.duty_cycle_sp = normalspeed
+        time.sleep(basetime)
+
+        leftlight = ls1.reflected_light_intensity
+        rightlight = ls2.reflected_light_intensity
+        if rightlight > schwarz and leftlight > schwarz:
+            turn()
 
 run()
